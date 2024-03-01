e97f7980f3167c708689a32682586a42db2f4ee9
mabdelwe <mouhamedvadel2021@mail.com>
2024-03-01 16:09:49 +0000

    creating a sql database table

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"github.com/fadel010/rest-api/db"
 	"github.com/fadel010/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
